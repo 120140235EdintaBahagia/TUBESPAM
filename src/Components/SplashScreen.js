b0c3778019eefbb29aa5bad5e6a0222459e74641
edintabahagia <103376672+edintabahagia@users.noreply.github.com>
2022-12-17 17:56:50 +0700

    Update SplashScreen.js

--- src/Components/SplashScreen.js
+++ src/Components/SplashScreen.js
@@ -1,38 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import React from "react";
-import { StyleSheet, Text, titleText, Image, View } from 'react-native';
+import React, { useEffect } from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
 
+const Splash = ({ navigation }) => {
+    
+    useEffect(() => {
+        setTimeout( () => {
+            navigation.replace('LoginScreen');
+        }, 3000)
+    }, [navigation]);
 
-export default function SplashScreen ({ navigation }) {
-  return (
-    <View style={styles.container}>
-      <Image style={styles.picture} source={require('./src/Images/Logo.jpeg')} />
-      <Text style={styles.baseText}>MENUWQQ</Text>
-        </View>
-  );
+    return (
+        <View style={styles.container}>
+        <Image style={styles.picture} source={require('../Images/Logo.png')} />
+        <Text style={styles.baseText}>MENUWQQ</Text>
+          </View>
+        
+    )
 }
 
-
+export default Splash
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#B8FCFC',
-    alignItems: 'center',
-  },
-  picture: {
-    resizeMode: "contain",
-    width: 290,
-    height: 216,
-    top : 300
-  },
-  baseText: {
-    fontSize: 20,
-    fontWeight: "bold",
-    width:110,
-    height:250,
-    top: 330
-  }
-});
-
-
-
+    container: {
+        flex: 1,
+        backgroundColor: '#B1E4D4',
+        alignItems: 'center',
+      },
+    picture: {
+        resizeMode: "contain",
+        width: 290,
+        height: 216,
+        top : 300
+      },
+      baseText: {
+        fontSize: 20,
+        fontWeight: "bold",
+        width:120,
+        height:250,
+        top: 330
+      }
+    });
