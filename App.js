33ef5962eae32256acf9aaa6dbb9a6ac0a1e22bf
Nadhif-92 <103347734+Nadhif-92@users.noreply.github.com>
2022-12-17 11:02:44 +0700

    Update App.js

--- App.js
+++ App.js
@@ -3,8 +3,9 @@ import { NavigationContainer } from '@react-navigation/native';
 import 'react-native-gesture-handler';
 
 import SplashScreen from './src/Components/SplashScreen';
-//import LoginScreen from './src/Components/LoginScreen';
-//import RegisterScreen from './src/Components/RegisterScreen'
+import LoginScreen from './src/Components/LoginScreen';
+import RegisterScreen from './src/Components/RegisterScreen';
+import Home from './src/Components/HomeScreen';
 
 const Stack = createStackNavigator();
 
@@ -13,6 +14,9 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="SplashScreen" component={SplashScreen} options={{headerShown: false,}}/>
+        <Stack.Screen name="LoginScreen" component={LoginScreen} options={{headerShown: false,}}/>
+        <Stack.Screen name="RegisterScreen" component={RegisterScreen} options={{headerShown: false,}}/>
+        <Stack.Screen name="HomeScreen" component={Home} options={{headerShown: false,}}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
