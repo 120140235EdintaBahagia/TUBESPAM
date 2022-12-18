2de1f309a0a43b80921a5fb38ec5a8e8660cf8fa
Nadhif-92 <103347734+Nadhif-92@users.noreply.github.com>
2022-12-18 18:00:38 +0700

    Update ProfilScreen.js

--- src/Components/ProfilScreen.js
+++ src/Components/ProfilScreen.js
@@ -1,19 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from "react";
 import { StyleSheet, Text, Image, View, TouchableOpacity } from 'react-native';
+import { EvilIcons, AntDesign, Ionicons } from '@expo/vector-icons';
 
-import { EvilIcons } from '@expo/vector-icons';
-
-function ProfilScreen(){
+function ProfilScreen({navigation}){
     return (
     <View style={styles.container}>
         <Image
           style={styles.tinyLogo}
           source={require('../Images/profillogin.png')}
           ></Image>
-      <View style = {{alignItems: "center", width:100}}>
+      <Text style = {styles.text1}>Profil Saya</Text>
+      <TouchableOpacity onPress={() => navigation.goBack()}>
+        <AntDesign name="arrowleft" size={30} color="black" style={styles.button1}></AntDesign>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate("LoginScreen")}>
+        <Ionicons name="log-out-outline" size={30} color="black" style={styles.button2}></Ionicons>
+      </TouchableOpacity>
+      <View style={styles.boxHijau}>
+        <Text style={styles.text2}>Sutomo Kartijo</Text>
+        <Text style={styles.text2}>tomo_kar@gmail.com</Text>
       </View>
-      <Text style = {{color: "black", fontSize: 25, fontWeight: 'bold', marginBottom:400}}>Profil Saya</Text>
     </View>
     );
 }
@@ -26,12 +33,40 @@ const styles = StyleSheet.create({
       justifyContent: "center",
     },
     tinyLogo: {
+        top: -50,
         width: 231,
         height:178,
-        marginBottom:-300
-      },
+        marginBottom:-280
+    },
+    text1:{
+        bottom: 10,
+        color: "black",
+        fontSize: 25,
+        fontWeight: 'bold',
+        marginBottom:500,
+        fontFamily: '',
+    },
+    text2:{
+        textAlign: "center",
+        color: "#E3F4EF",
+        fontSize: 25,
+        top: 15,
+    },
+    button1 :{
+        bottom: 541,
+        right : 159,
+    },
+    button2 :{
+        bottom: 574,
+        left : 159,
+    },
+    boxHijau:{
+        backgroundColor: "#3ABD8F",
+        width: '90%',
+        height: 120,
+        borderRadius: 15,
+        position: "absolute",
+    }
   });
-
-
 
 export default ProfilScreen;
