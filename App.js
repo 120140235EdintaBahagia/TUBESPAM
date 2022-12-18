70e0b19a6f1456e383f890cdcaf1fbaa835d70d4
edintabahagia <103376672+edintabahagia@users.noreply.github.com>
2022-12-18 16:49:32 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View , Image} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -8,14 +8,10 @@ import SplashScreen from './src/Components/SplashScreen';
 import HomeScreen from './src/Components/HomeScreen';
 import LoginScreen from './src/Components/LoginScreen';
 import RegisterScreen from './src/Components/RegisterScreen';
+import ProfilScreen from './src/Components/ProfilScreen'
+import FavoritScreen from './src/Components/FavoritScreen';
+import KotakScreen from './src/Components/KotakScreen';
 
-function SettingsScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
-    </View>
-  );
-}
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
@@ -24,7 +20,9 @@ function Bottom() {
   return (
     <Tab.Navigator>
       <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Settings" component={SettingsScreen} />
+      <Tab.Screen name="Favorit" component={FavoritScreen}/>
+      <Tab.Screen name="Kotak" component={KotakScreen} />
+      <Tab.Screen name="Profil" component={ProfilScreen} />
     </Tab.Navigator>
   );
 }
